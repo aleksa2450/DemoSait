626777c3d2554b88cada714bb46536b79cc73954
aleksa2450 <aleksa2450@yandex.ru>
2022-03-30 02:38:19 +0300

    Шаблон начальной страницы

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
 <?php
 declare(strict_types=1);
 error_reporting(-1);
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>DEMO-SITE</title>
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand">DEMO-SITE</a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Главаная</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Категории</a>
+            </li>
+
+        </ul>
+
+    </div>
+
+</nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+</body>
+</html>
